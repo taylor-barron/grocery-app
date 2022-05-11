c2ef080bd77ccfa96d289303d4b4bb4e4dd1d386
taylor-barron <taylor.barron989@gmail.com>
2022-05-11 15:14:39 -0400

    button added

--- grocery-app/src/App.js
+++ grocery-app/src/App.js
@@ -1,21 +1,13 @@
-import './App.css';
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Button from './components/Button'
+import Header from './components/Button'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button color="red" text="hi" />
+    
     </div>
   );
 }
